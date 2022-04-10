cd3b28160cb7dd95cc8a5c8205d90d6d3182e232
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-04-10 09:43:49 +0530

    redux stucture done for paypal button

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from './Reducers/product.reduces'
 import { cartReducer } from './Reducers/cart.reducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpadateProfileReducer } from './Reducers/user.reducers'
-import { orderCreateReducer, orderDetailsReducer } from './Reducers/order.reducers'
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './Reducers/order.reducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -15,7 +15,8 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpadateProfileReducer,
     orderCreate: orderCreateReducer,
-    orderDetails: orderDetailsReducer
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
