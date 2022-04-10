e60f2e45893a4be3313f59b8b86a45940b157a35
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-04-10 10:49:20 +0530

    payment integrated

--- Backend/server.js
+++ Backend/server.js
@@ -20,10 +20,12 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('API is running')
 })
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+
 
 app.use(notFound)
 app.use(errorHandler)
